fec04f6724eb25de9dd06ec55f2ab30e7c564457
Dương Đỗ <dustin.do95@gmail.com>
2019-03-10 13:56:05 +0700

    feat: format "Hello world" using inline-style

--- src/App.js
+++ src/App.js
@@ -2,7 +2,23 @@ import React, { Component } from 'react'
 
 class App extends Component {
   render() {
-    return <div>Hello world</div>
+    return (
+      <div
+        style={{
+          height: '100vh',
+          width: '100vw',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          flexDirection: 'column',
+        }}
+      >
+        <div>Hello world</div>
+        <div>
+          This is my first <strong>React.js</strong> app
+        </div>
+      </div>
+    )
   }
 }
 
