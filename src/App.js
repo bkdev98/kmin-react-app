21ce1716b4036310e55f2278de00210ab7bd427d
Dương Đỗ <dustin.do95@gmail.com>
2019-03-10 14:21:49 +0700

    feat(count): add counter with increase, decrease, auto increment

--- src/App.js
+++ src/App.js
@@ -1,21 +1,52 @@
 import React, { Component } from 'react'
 
 class App extends Component {
+  state = {
+    value: 0,
+    autoIncrement: false,
+  }
+
+  handleIncrease = () => this.setState({ value: this.state.value + 1 })
+  handleDecrease = () => this.setState({ value: this.state.value - 1 })
+  handleReset = () => this.setState({ value: 0 })
+
+  handleToggleAutoIncrement = () => {
+    const nextValue = !this.state.autoIncrement
+
+    if (nextValue) {
+      this.autoIncrementInterval = setInterval(this.handleIncrease, 500)
+    } else {
+      clearInterval(this.autoIncrementInterval)
+    }
+    this.setState({ autoIncrement: !this.state.autoIncrement })
+  }
+
   render() {
+    const { value, autoIncrement } = this.state
+
     return (
-      <div
-        style={{
-          height: '100vh',
-          width: '100vw',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          flexDirection: 'column',
-        }}
-      >
-        <div>Hello world</div>
+      <div>
         <div>
-          This is my first <strong>React.js</strong> app
+          Current value:{' '}
+          <span
+            style={{
+              color:
+                value < 0 ? 'orangered' : value === 0 ? 'initial' : 'green',
+            }}
+          >
+            {value}
+          </span>
+        </div>
+        <button onClick={this.handleIncrease}>Increase</button>
+        <button onClick={this.handleDecrease}>Decrease</button>
+        <button onClick={this.handleReset}>Reset</button>
+        <div>
+          <input
+            type="checkbox"
+            checked={autoIncrement}
+            onChange={this.handleToggleAutoIncrement}
+          />
+          Auto increment
         </div>
       </div>
     )
